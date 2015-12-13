fa141911472eff8ff6546523f8ad5502f17d915a
Kyungil Choi <hanpama@gmail.com>
2015-12-13 21:53:07 +0900

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='peppertext',
-    version="0.1.1",
+    version="0.1.2",
     description='Declarative hypertext client',
     author='Kyungil Choi',
     author_email='hanpama@gmail.com',
