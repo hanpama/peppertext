ec8a5cb612c85ce30d969947db5e796718ef81c3
Kyungil Choi <hanpama@gmail.com>
2015-12-13 21:47:08 +0900

    Remove python2.7 classifier

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.0",
         "Programming Language :: Python :: 3.1",
